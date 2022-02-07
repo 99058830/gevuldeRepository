3f87cc80957b2187a14ceaf9054aaf6b2310013f
99058830 <99058830@mydavinci.nl>
2022-02-07 14:42:57 +0100

    fixed turn on and off

--- lightswitch.py
+++ lightswitch.py
@@ -1,13 +1,20 @@
 import tkinter as tk
 window = tk.Tk()
 
-button = tk.Button(text='...', bg="white", fg="black")
+# schijf hier tussen je code
+
+def clickyOnOff():
+    if button.config('text')[-1] == 'Switch light off':
+        button.config(text='Switch light on', bg='white', fg='black')
+        window.config(bg='black')
+    else:
+        button.config(text='Switch light off', bg='white', fg='black')
+        window.config(bg='yellow')
+
+# schijf hier tussen je code
+
+button = tk.Button(window, text='Switch light on', bg="white", fg="black", command=clickyOnOff)
 button.pack(pady = 20, padx = 20)
-
-# schijf hier tussen je code
-
-
-
-# schijf hier tussen je code
+window.config(bg='black')
 
 window.mainloop()
